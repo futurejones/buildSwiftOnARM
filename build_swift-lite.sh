ed56b818631e53f27c7117374aac5e59ca633851
Neil Jones <futurejones@users.noreply.github.com>
2019-10-31 10:15:16 +0900

    Update and rename build.sh to build_swift-lite.sh

--- build_swift-lite.sh
+++ build_swift-lite.sh
@@ -12,8 +12,8 @@ rm -rf $INSTALL_DIR $PACKAGE
     -R \
     --build-subdir buildbot_linux --install-prefix=/usr \
     --install-destdir=${INSTALL_DIR} --installable-package=${PACKAGE} \
-    --lldb --llbuild --xctest --swiftpm --foundation --libdispatch \
-    -- --install-libdispatch --install-foundation --install-swift --install-lldb --install-llbuild --install-xctest --install-swiftpm \
+    --foundation --libdispatch \
+    -- --install-libdispatch --install-foundation --install-swift \
     '--swift-install-components=autolink-driver;compiler;clang-builtin-headers;stdlib;swift-remote-mirror;sdk-overlay;dev' \
     --build-swift-static-stdlib --build-swift-static-sdk-overlay
 
