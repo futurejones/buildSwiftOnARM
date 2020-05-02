4ed77382eb7ed69bd292431be9b93f98098449e0
futurejones <futurejones@users.noreply.github.com>
2020-05-03 08:49:00 +0900

    add swift 5.2

---
+++ swift-5.2-RELEASE/clone_5.2_RELEASE.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+. "$(dirname $0)/utils.sh"
+
+# Distributions derived by Stretch need an older clang, use the default for the rest
+if [ $VERSION = "stretch" ] || [ $VERSION = "16.04" ]; then
+    CLANG_VERSION=-3.8
+fi
+
+# Basic dependencies
+echo "✅ Installing dependencies..."
+sudo apt-get install -y nano htop make patch rsync git cmake ninja-build clang$CLANG_VERSION python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config libblocksruntime-dev libcurl4-openssl-dev autoconf libtool systemtap-sdt-dev libcurl4-openssl-dev libz-dev
+
+# Some OS or specific versions could have additional requirements, if they do
+# a script will be present in distro-scripts and we'll run it right after the more generic configuration
+for VARIANT in $OS $VERSION $OS$VERSION
+do 
+    if [ -f ./distro-scripts/$VARIANT.sh ]; then
+        echo "✅ Running configuration script for $VARIANT..."
+        ./distro-scripts/$VARIANT.sh
+    fi   
+done 
+
+# Clone Swift and all the related projects
+git clone https://github.com/apple/swift
+cd swift
+git checkout swift-5.2.3-RELEASE
+cd -
+./swift/utils/update-checkout --clone --tag swift-5.2.3-RELEASE
